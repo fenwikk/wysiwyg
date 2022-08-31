b5171d2b630f150bc6fee92709f1626226ddb2dc
Filip Mårtensson <fenwikk@protonmail.com>
2022-08-31 21:18:07 +0200

    Fixed not sticky modal

--- src/components/EditModal.svelte
+++ src/components/EditModal.svelte
@@ -16,15 +16,13 @@ const save = () => {
 </script>
 
 {#if $editingKey != ""}
-    <div class="absolute h-screen w-full z-50">
+    <div class="fixed top-0 h-screen w-full z-50 flex justify-center items-center">
         <div class="z-40 absolute h-full w-full bg-gray-800/50" on:click={() => editingKey.set("")} />
-        <div class="z-50 absolute h-full w-full flex justify-center items-center">
-            <div class="bg-white p-6 rounded-md">
-                <h3 class="text-xl">Editing {$editingKey}</h3>
-                <input class="bg-gray-400" type="text" on:change={(e) => temp = e.currentTarget.value} value={$pageData[$editingKey].string}>
+        <div class="bg-white p-6 rounded-md z-50">
+            <h3 class="text-xl">Editing {$editingKey}</h3>
+            <input class="bg-gray-400" type="text" on:change={(e) => temp = e.currentTarget.value} value={$pageData[$editingKey].string}>
 
-                <button on:click={save}>Save</button>
-            </div>
+            <button on:click={save}>Save</button>
         </div>
     </div>
 {/if}
